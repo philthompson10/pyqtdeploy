e094e9cc18c07f737074934ada40a84b45177fd7
Phil Thompson <phil@riverbankcomputing.com>
2020-07-14 11:22:40 +0100

    Removed support for versions of Python earlier than v3.5 from the builder library.

--- pyqtdeploy/builder/lib/pyqtdeploy_start.cpp
+++ pyqtdeploy/builder/lib/pyqtdeploy_start.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, Riverbank Computing Limited
+// Copyright (c) 2020, Riverbank Computing Limited
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -35,57 +35,29 @@
 #include <QTextCodec>
 
 #include "frozen_bootstrap.h"
-
-#if PY_VERSION_HEX >= 0x03050000
 #include "frozen_bootstrap_external.h"
-#endif
 
 #if defined(PYQTDEPLOY_FROZEN_MAIN)
 #include "frozen_main.h"
 #endif
 
 
-#if PY_MAJOR_VERSION >= 3
-
-#define BOOTSTRAP_MODULE            "_frozen_importlib"
-#define BOOTSTRAP_EXTERNAL_MODULE   "_frozen_importlib_external"
-#define PDYTOOLS_INIT               PyInit_pdytools
-#define CONST_CAST(s)               s
 extern "C" PyObject *PyInit_pdytools(void);
-
-#if defined(Q_OS_WIN)
-#define WIDE_ARGV
-#endif
-
-#else
-
-#define BOOTSTRAP_MODULE    "__bootstrap__"
-#define PDYTOOLS_INIT       initpdytools
-#define CONST_CAST(s)       const_cast<char *>(s)
-extern "C" void initpdytools(void);
-
-#endif
-
 
 // The internal API.
 void pdytools_init_executable_dir(const QString &argv0);
 const QDir &pdytools_get_executable_dir();
 
-
 // We use Qt as the source of the locale information, partly because it
 // officially supports Android.
 static QTextCodec *locale_codec;
 
-
 // Foward declarations.
 static int handle_exception();
 static int append_path_dirs(PyObject *list, const char **path_dirs);
-#if PY_MAJOR_VERSION < 3
-static PyObject *string_from_qstring(const QString &qs);
-#endif
 
 
-#if defined(WIDE_ARGV)
+#if defined(Q_OS_WIN)
 int pyqtdeploy_start(int argc, wchar_t **w_argv,
         struct _inittab *extension_modules, const char *main_module,
         const char *entry_point, const char **path_dirs)
@@ -98,20 +70,18 @@ int pyqtdeploy_start(int argc, char **argv,
     // The replacement table of frozen modules.
     static struct _frozen modules[] = {
         {
-            CONST_CAST(BOOTSTRAP_MODULE),
+            "_frozen_importlib",
             frozen_pyqtdeploy_bootstrap,
             sizeof (frozen_pyqtdeploy_bootstrap)
         },
-#if PY_VERSION_HEX >= 0x03050000
         {
-            CONST_CAST(BOOTSTRAP_EXTERNAL_MODULE),
+            "_frozen_importlib_external",
             frozen_pyqtdeploy_bootstrap_external,
             sizeof (frozen_pyqtdeploy_bootstrap_external)
         },
-#endif
 #if defined(PYQTDEPLOY_FROZEN_MAIN)
         {
-            CONST_CAST("__main__"),
+            "__main__",
             frozen_pyqtdeploy_main,
             sizeof (frozen_pyqtdeploy_main)
         },
@@ -124,7 +94,7 @@ int pyqtdeploy_start(int argc, char **argv,
 
     if (!locale_codec)
     {
-#if defined(WIDE_ARGV)
+#if defined(Q_OS_WIN)
         fwprintf(stderr, L"%s: no locale codec found\n", w_argv[0]);
 #else
         fprintf(stderr, "%s: no locale codec found\n", argv[0]);
@@ -140,7 +110,6 @@ int pyqtdeploy_start(int argc, char **argv,
     Py_OptimizeFlag = 1;
 #endif
 
-#if PY_MAJOR_VERSION >= 3
     if (!Py_FileSystemDefaultEncoding)
     {
         // Python doesn't have a platform default so get it from Qt.  However
@@ -155,14 +124,13 @@ int pyqtdeploy_start(int argc, char **argv,
             Py_HasFileSystemDefaultEncoding = 1;
         }
     }
-#endif
 
     PyImport_FrozenModules = modules;
 
     // Add the importer to the table of builtins.
-    if (PyImport_AppendInittab("pdytools", PDYTOOLS_INIT) < 0)
+    if (PyImport_AppendInittab("pdytools", PyInit_pdytools) < 0)
     {
-#if defined(WIDE_ARGV)
+#if defined(Q_OS_WIN)
         fwprintf(stderr, L"%s: PyImport_AppendInittab() failed\n", w_argv[0]);
 #else
         fprintf(stderr, "%s: PyImport_AppendInittab() failed\n", argv[0]);
@@ -174,7 +142,7 @@ int pyqtdeploy_start(int argc, char **argv,
     if (extension_modules != NULL)
         if (PyImport_ExtendInittab(extension_modules) < 0)
         {
-#if defined(WIDE_ARGV)
+#if defined(Q_OS_WIN)
             fwprintf(stderr, L"%s: PyImport_ExtendInittab() failed\n", w_argv[0]);
 #else
             fprintf(stderr, "%s: PyImport_ExtendInittab() failed\n", argv[0]);
@@ -182,8 +150,7 @@ int pyqtdeploy_start(int argc, char **argv,
             return 1;
         }
 
-#if PY_MAJOR_VERSION >= 3
-#if !defined(WIDE_ARGV)
+#if !defined(Q_OS_WIN)
     // Convert the argument list to wide characters using the locale codec.
     wchar_t **w_argv = new wchar_t *[argc + 1];
 
@@ -205,32 +172,18 @@ int pyqtdeploy_start(int argc, char **argv,
     Py_SetProgramName(w_argv[0]);
     Py_Initialize();
     PySys_SetArgvEx(argc, w_argv, 0);
-#else
-    // Initialise the Python v2 interpreter.
-    Py_SetProgramName(argv[0]);
-    Py_Initialize();
-    PySys_SetArgvEx(argc, argv, 0);
-
-    // Initialise the path hooks.
-    if (PyImport_ImportFrozenModule(CONST_CAST(BOOTSTRAP_MODULE)) < 0)
-        return handle_exception();
-#endif
 
     // Set sys.frozen.
-    if (PySys_SetObject(CONST_CAST("frozen"), Py_True) < 0)
+    if (PySys_SetObject("frozen", Py_True) < 0)
         return handle_exception();
 
     // Initialise the directory containing the executable.
-#if PY_MAJOR_VERSION >= 3
     pdytools_init_executable_dir(QString::fromWCharArray(w_argv[0]));
-#else
-    pdytools_init_executable_dir(locale_codec->toUnicode(argv[0]));
-#endif
 
     // Configure sys.path.
     if (path_dirs != NULL)
     {
-        PyObject *py_path = PySys_GetObject(CONST_CAST("path"));
+        PyObject *py_path = PySys_GetObject("path");
 
         if (py_path)
         {
@@ -250,12 +203,7 @@ int pyqtdeploy_start(int argc, char **argv,
 
     mod_dict = PyModule_GetDict(mod);
 
-#if PY_MAJOR_VERSION >= 3
     py_filename = PyUnicode_FromString(":/__main__.pyo");
-#else
-    py_filename = PyString_FromString(":/__main__.pyo");
-#endif
-
     if (py_filename == NULL)
         return handle_exception();
 
@@ -265,18 +213,13 @@ int pyqtdeploy_start(int argc, char **argv,
     Py_DECREF(py_filename);
 
     // Import the main module.
-    if (PyImport_ImportFrozenModule(CONST_CAST(main_module)) < 0)
+    if (PyImport_ImportFrozenModule(main_module) < 0)
         return handle_exception();
 #else
     // Import the main module.
     PyObject *mod, *main_module_obj;
 
-#if PY_MAJOR_VERSION >= 3
     main_module_obj = PyUnicode_FromString(main_module);
-#else
-    main_module_obj = string_from_qstring(QString::fromUtf8(main_module));
-#endif
-
     if (main_module_obj == NULL)
         return handle_exception();
 
@@ -285,7 +228,7 @@ int pyqtdeploy_start(int argc, char **argv,
         return handle_exception();
 
     // Call the entry point.
-    if (!PyObject_CallMethod(mod, CONST_CAST(entry_point), NULL))
+    if (!PyObject_CallMethod(mod, entry_point, NULL))
         return handle_exception();
 #endif
 
@@ -324,19 +267,11 @@ static int handle_exception()
                     exit_code = 0;
                     Py_DECREF(code);
                 }
-#if PY_MAJOR_VERSION >= 3
                 else if (PyLong_Check(code))
                 {
                     exit_code = (int)PyLong_AsLong(code);
                     Py_DECREF(code);
                 }
-#else
-                else if (PyInt_Check(code))
-                {
-                    exit_code = (int)PyInt_AsLong(code);
-                    Py_DECREF(code);
-                }
-#endif
                 else
                 {
                     exit_code = 1;
@@ -404,12 +339,8 @@ static int append_path_dirs(PyObject *list, const char **path_dirs)
         // Convert to a Python string.
         PyObject *py_path_dir;
 
-#if PY_MAJOR_VERSION >= 3
         QByteArray utf8(path_dir.toUtf8());
         py_path_dir = PyUnicode_FromStringAndSize(utf8.data(), utf8.length());
-#else
-        py_path_dir = string_from_qstring(path_dir);
-#endif
 
         if (!py_path_dir)
             return -1;
@@ -424,14 +355,3 @@ static int append_path_dirs(PyObject *list, const char **path_dirs)
 
     return 0;
 }
-
-
-#if PY_MAJOR_VERSION < 3
-// Convert a QString to a Python v2 locale encoded str object.
-static PyObject *string_from_qstring(const QString &qs)
-{
-    QByteArray locale_s(locale_codec->fromUnicode(qs));
-
-    return PyString_FromStringAndSize(locale_s, locale_s.length());
-}
-#endif
