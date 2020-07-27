8bf12448c9e718e8c072e96587cb8d7aa887726d
Phil Thompson <phil@riverbankcomputing.com>
2020-07-27 16:21:54 +0100

    Improve the error handling of the GUI application.

--- pyqtdeploy/gui/filename_editor.py
+++ pyqtdeploy/gui/filename_editor.py
@@ -85,7 +85,7 @@ class FilenameEditor(QWidget):
 
         orig = default = self._line_edit.text()
         if default != '' and self._project is not None:
-            default = self._project.path_from_user(default)
+            default = self._project.project_path(default)
 
         if self._directory:
             name = QFileDialog.getExistingDirectory(self._line_edit,
@@ -96,7 +96,7 @@ class FilenameEditor(QWidget):
 
         if name != '':
             if self._project is not None:
-                name = self._project.path_to_user(name)
+                name = self._project.minimal_path(name)
 
             if name != orig:
                 self._line_edit.setText(name)
