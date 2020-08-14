2a2ec63d90241b84637f6e6b8c6b154b6dd7cb72
Phil Thompson <phil@riverbankcomputing.com>
2020-08-14 14:45:49 +0100

    Improve the help of pyqtdeploy-build.

--- pyqtdeploy/pyqtdeploybuild_main.py
+++ pyqtdeploy/pyqtdeploybuild_main.py
@@ -51,10 +51,10 @@ def main():
     parser.add_argument('--python',
             help="the python executable when using an existing Python "
                     "installation",
-            metavar="FILE")
+            metavar="EXECUTABLE")
     parser.add_argument('--qmake',
             help="the qmake executable when using an existing Qt installation",
-            metavar="FILE")
+            metavar="EXECUTABLE")
     parser.add_argument('--resources',
             help="the number of .qrc resource files to generate [default: 1]",
             metavar="NUMBER", type=int, default=1),
