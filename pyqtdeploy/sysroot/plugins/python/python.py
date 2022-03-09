0579dad1f684080487561b8a66ba5479cb9a067d
Phil Thompson <phil@riverbankcomputing.com>
2022-03-09 16:38:21 +0000

    Added support for Python v3.8.12.

--- pyqtdeploy/sysroot/plugins/python/python.py
+++ pyqtdeploy/sysroot/plugins/python/python.py
@@ -36,7 +36,7 @@ from .standard_library import standard_library
 
 # The latest tested patch releases of each minor version.
 LATEST_3_7_RELEASE = (3, 7, 12)
-LATEST_3_8_RELEASE = (3, 8, 11)
+LATEST_3_8_RELEASE = (3, 8, 12)
 
 
 class PythonComponent(AbstractPythonComponent):
