15ed493a679a9f1cb9592243364d6cb6f47aea1d
Phil Thompson <phil@riverbankcomputing.com>
2022-03-12 17:12:03 +0000

    Added support fro Python v3.9.4.

--- pyqtdeploy/sysroot/plugins/python/python.py
+++ pyqtdeploy/sysroot/plugins/python/python.py
@@ -37,7 +37,7 @@ from .standard_library import standard_library
 # The latest tested patch releases of each minor version.
 LATEST_3_7_RELEASE = (3, 7, 12)
 LATEST_3_8_RELEASE = (3, 8, 12)
-LATEST_3_9_RELEASE = (3, 9, 2)
+LATEST_3_9_RELEASE = (3, 9, 4)
 
 
 class PythonComponent(AbstractPythonComponent):
@@ -184,6 +184,10 @@ class PythonComponent(AbstractPythonComponent):
             if self.version > LATEST_3_8_RELEASE:
                 self.untested()
         elif self.version == (3, 9):
+            # This version was withdrawn.
+            if self.version == (3, 9, 3):
+                self.unsupported()
+
             if self.version > LATEST_3_9_RELEASE:
                 self.untested()
         else:
