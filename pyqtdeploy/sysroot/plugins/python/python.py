03dc9686dd06c8f4893dde4a937f89f59edb3c92
Phil Thompson <phil@riverbankcomputing.com>
2022-09-09 09:50:19 +0100

    Added support for Python v3.10.7, v3.9.14, v3.8.14 and v3.7.14.

--- pyqtdeploy/sysroot/plugins/python/python.py
+++ pyqtdeploy/sysroot/plugins/python/python.py
@@ -39,10 +39,10 @@ from .configurations import pyconfig as pyconfig_package
 
 
 # The latest tested patch releases of each minor version.
-LATEST_3_7_RELEASE = (3, 7, 13)
-LATEST_3_8_RELEASE = (3, 8, 13)
-LATEST_3_9_RELEASE = (3, 9, 13)
-LATEST_3_10_RELEASE = (3, 10, 6)
+LATEST_3_7_RELEASE = (3, 7, 14)
+LATEST_3_8_RELEASE = (3, 8, 14)
+LATEST_3_9_RELEASE = (3, 9, 14)
+LATEST_3_10_RELEASE = (3, 10, 7)
 
 
 class PythonComponent(AbstractPythonComponent):
